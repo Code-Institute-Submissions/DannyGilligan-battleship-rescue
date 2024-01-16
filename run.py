cf367008317e5d0cc36e093fe74d0a4ad54f10a4
DannyGilligan <gilligan.danny@gmail.com>
2024-01-16 23:37:38 +0000

    Update invalid difficulty alert text

--- run.py
+++ run.py
@@ -97,8 +97,8 @@ def mission_difficulty_screen():
         clearscreen()
         print(banner_art)
         mission_difficulty = input('\n\n       \033[33mAlert!!\033[0m ' + \
-        'Enter Mission Difficulty (\x1b[33mCadet\033[0m, \x1b[33mCaptain\033[0m,' + \
-        ' \x1b[33mAdmiral\033[0m) \n\n                                   ')
+        'Enter Mission Difficulty (Type \x1b[33mCadet\033[0m, \x1b[33mCaptain\033[0m' + \
+        'or \x1b[33mAdmiral\033[0m) \n\n                                   ')
         mission_difficulty = mission_difficulty.lower()
     clearscreen()
 
