1c456870b58828ccbab974a8dc881828ae1f91f4
DannyGilligan <gilligan.danny@gmail.com>
2024-01-16 22:20:41 +0000

    remove line breaks from start screen

--- run.py
+++ run.py
@@ -251,8 +251,8 @@ print(banner_art_lower) # Prints 'Rescue' to screen
 #print('\033[47m\033[31m\033[1m MERCHANT SHIP DESTROYED \033[0m\033[22m')
 
 sleep(0.6)
-typing_effect('\n\n                         The enemy controls the Land.\n\n', 0.03)
-typing_effect('                         The enemy controls the Skies.\n\n', 0.03)
+typing_effect('\n                         The enemy controls the Land.\n', 0.03)
+typing_effect('                         The enemy controls the Skies.\n', 0.03)
 typing_effect('            One Battleship stands between them controlling the Seas.\n', 0.03)
 sleep(0.4)
 username_prompt = typing_effect('\n            Enter your callsign below (between 2 and 15 characters):\n\n', 0.03) # Requests user to input 'callsign'
