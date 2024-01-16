592871030d7c38a1c20383dcc609db35d86cbf6d
DannyGilligan <gilligan.danny@gmail.com>
2024-01-16 22:55:27 +0000

    Fix formatting of mission difficulty options text

--- run.py
+++ run.py
@@ -98,7 +98,7 @@ def mission_difficulty_screen():
         print(banner_art)
         mission_difficulty = input('\n\n       \033[33mAlert!!\033[0m ' + \
         'Enter Mission Difficulty (\x1b[33mCadet\033[0m, \x1b[33mCaptain\033[0m,' + \
-        ' \x1b[33mAdmiral)\033[0m \n\n                                   ')
+        ' \x1b[33mAdmiral\033[0m) \n\n                                   ')
         mission_difficulty = mission_difficulty.lower()
     clearscreen()
 
