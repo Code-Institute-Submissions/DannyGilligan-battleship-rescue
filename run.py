9409fd07168a87a6cf066e0191c3f8304e97c835
DannyGilligan <gilligan.danny@gmail.com>
2024-01-17 10:09:39 +0000

    Add start screen function

--- run.py
+++ run.py
@@ -4,13 +4,16 @@
 
 
 
-########## Imports ##########
+# >>>>>>>>>>>>>>> Imports <<<<<<<<<<<<<<<
+
 import os
 from random import randint
 import sys
 import time
 from time import sleep
 
+
+# >>>>>>>>>>>>>>> Variables <<<<<<<<<<<<<<<
 
 # Stores a valid username
 username = None
@@ -45,8 +48,68 @@ battleship_hull_integrity = 100
 # Stores torpedo's available to user
 torpedo_count = 40
 
+# Stores the 'Battleship' portion of the banner art
+banner_art_upper = ('''
+             ___   ___  ______ ______ __    ____ ____ __ __ ____ ___   
+            / _ ) / _ |/_  __//_  __// /   / __// __// // //  _// _ \ 
+           / _  |/ __ | / /    / /  / /__ / _/ _\ \ / _  /_/ / / ___/
+          /____//_/ |_|/_/    /_/  /____//___//___//_//_//___//_/    
+''')
 
-def typing_effect(text, speed):   
+# Stores the 'Rescue' portion of the banner art
+banner_art_lower = ('''
+                         ___   ____ ____ _____ __  __ ____
+                        / _ \ / __// __// ___// / / // __/
+                       / , _// _/ _\ \ / /__ / /_/ // _/ 
+                      /_/|_|/___//___/ \___/ \____//___/
+''')
+
+
+# Stores the banner art in its entirety
+banner_art = ('''
+             ___   ___  ______ ______ __    ____ ____ __ __ ____ ___   
+            / _ ) / _ |/_  __//_  __// /   / __// __// // //  _// _ \ 
+           / _  |/ __ | / /    / /  / /__ / _/ _\ \ / _  /_/ / / ___/
+          /____//_/ |_|/_/    /_/  /____//___//___//_//_//___//_/    
+
+\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m
+\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m
+\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m
+                         ___   ____ ____ _____ __  __ ____
+                        / _ \ / __// __// ___// / / // __/
+                       / , _// _/ _\ \ / /__ / /_/ // _/ 
+                      /_/|_|/___//___/ \___/ \____//___/
+
+''')
+
+# Stores the mission details message at the mission accept screen
+mission_details_message = ('''It is with great regret that I must inform you that the situation has
+    become dire. Our forces are being routed in every theatre and on every
+    front, our supply chains have been decimated and our ability to sustain
+    a defence against this onslaught can be measured now in hours.\n
+    Your mission is to intercept and eliminate a fleet of 5 enemy Destroyers
+    currently pursuing 2 friendly Merchant ships sailing for our Capital Port. 
+    These Merchant ships are on a clandestine mission to deliver classified 
+    cargo that will turn the tide of this war once and for all!\n
+    Unfortunately, during a recent skirmish, the Merchant ships lost all
+    communication capabilities and the enemy's radar jamming technology is
+    preventing us from locating them. But we know they're out there, somewhere.
+    Hunt down the enemy with extreme prejudice, avoid friendly fire at all
+    costs and rescue those Merchant ships.\n
+    Losing that cargo, means losing the war!\n''')
+
+
+
+# >>>>>>>>>>>>>>> Functions <<<<<<<<<<<<<<<
+
+def typing_effect(text, speed):
+    """
+    The typing effect functions will provide a visual
+    effect that mimics the text being 'typed' in real
+    time across the screen. This functions has been
+    adapted from a Stackoverflow post credited in the 
+    README.md file
+    """ 
     words = text
     for char in words:
         time.sleep(speed) #amended to make speed a parameter
@@ -75,12 +138,10 @@ def validate_username_screen():
     global username
     username = username.strip()
 
-
-    """
     # While loop is used to perform validation on the username input,
     # must be greater than 2 characters and less than 15. The while 
     # loop will display an alert message until the parameters are satisfied.
-    """
+    
     while len(username) < 2 or len(username) > 15:
         clearscreen() # Screen is cleared
         print(banner_art) 
@@ -124,11 +185,19 @@ def mission_difficulty_screen():
 
 
 def mission_accept_screen():
+    """
+    The mission accept screen function will provide a 
+    visual effect where the user is connecting to 'Central Command'
+    in order to access a message containing the mission details.
+    This message will then display an input field requesting the 
+    user to accept or reject.
+    """
+
     clearscreen()
     time.sleep(.4)
     print('\033[33m*******************************|Battleship Comms|*******************************\033[0m')
     print(''' \n\n\n\n\n\n\n\n\n\n              Establishing secure connection with Central Command.''')
-    time.sleep(1)
+    time.sleep(.4)
     clearscreen()
     print('\033[33m*******************************|Battleship Comms|*******************************\033[0m')
     print(''' \n\n\n\n\n\n\n\n\n\n              Establishing secure connection with Central Command..''')
@@ -210,68 +279,44 @@ def mission_accept_screen():
 
 
 
-banner_art_upper = ('''
-             ___   ___  ______ ______ __    ____ ____ __ __ ____ ___   
-            / _ ) / _ |/_  __//_  __// /   / __// __// // //  _// _ \ 
-           / _  |/ __ | / /    / /  / /__ / _/ _\ \ / _  /_/ / / ___/
-          /____//_/ |_|/_/    /_/  /____//___//___//_//_//___//_/    
-''')
-
-banner_art_lower = ('''
-                         ___   ____ ____ _____ __  __ ____
-                        / _ \ / __// __// ___// / / // __/
-                       / , _// _/ _\ \ / /__ / /_/ // _/ 
-                      /_/|_|/___//___/ \___/ \____//___/
-''')
-
-banner_art = ('''
-             ___   ___  ______ ______ __    ____ ____ __ __ ____ ___   
-            / _ ) / _ |/_  __//_  __// /   / __// __// // //  _// _ \ 
-           / _  |/ __ | / /    / /  / /__ / _/ _\ \ / _  /_/ / / ___/
-          /____//_/ |_|/_/    /_/  /____//___//___//_//_//___//_/    
-
-\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m
-\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m
-\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m
-                         ___   ____ ____ _____ __  __ ____
-                        / _ \ / __// __// ___// / / // __/
-                       / , _// _/ _\ \ / /__ / /_/ // _/ 
-                      /_/|_|/___//___/ \___/ \____//___/
-
-''')
 
 
 
 
 
-sleep(0.4)
-print(banner_art_upper) # Prints 'Battleship' to screen
+def start_screen:
+    """
+    The start screen function displays the initial welcome to
+    the user along with the banner art and callsign input field
+    """
+    sleep(0.4)
+    print(banner_art_upper) # Prints 'Battleship' to screen
 
-typing_effect\
-('\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m\n', 0.005)
-#sleep(0.1)
-typing_effect\
-('\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m\n', 0.005)
-#sleep(0.1)
-typing_effect\
-('\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m', 0.005)
+    typing_effect\
+    ('\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m\n', 0.005)
+    #sleep(0.1)
+    typing_effect\
+    ('\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m\n', 0.005)
+    #sleep(0.1)
+    typing_effect\
+    ('\x1b[96m〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜〜\033[0m', 0.005)
 
-print(banner_art_lower) # Prints 'Rescue' to screen
+    print(banner_art_lower) # Prints 'Rescue' to screen
 
-# Effects to be used for shot confirmations
-#typing_effect('\n\n\n\033[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m   ', 0.005)
-#print('\033[41m\033[37m\033[1m      BATTLESHIP HIT     \033[0m\033[22m')
-#print('\033[42m\033[30m\033[1m ENEMY SHIP NEUTRALISED  \033[0m\033[22m')
-#print('\033[47m\033[30m\033[1m      TARGET MISSED      \033[0m\033[22m')
-#print('\033[47m\033[31m\033[1m MERCHANT SHIP DESTROYED \033[0m\033[22m')
+    # Effects to be used for shot confirmations
+    #typing_effect('\n\n\n\033[36m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m   ', 0.005)
+    #print('\033[41m\033[37m\033[1m      BATTLESHIP HIT     \033[0m\033[22m')
+    #print('\033[42m\033[30m\033[1m ENEMY SHIP NEUTRALISED  \033[0m\033[22m')
+    #print('\033[47m\033[30m\033[1m      TARGET MISSED      \033[0m\033[22m')
+    #print('\033[47m\033[31m\033[1m MERCHANT SHIP DESTROYED \033[0m\033[22m')
 
-sleep(0.6)
-typing_effect('                         The enemy controls the Land.\n', 0.03)
-typing_effect('                         The enemy controls the Skies.\n', 0.03)
-typing_effect('            One Battleship stands between them controlling the Seas.\n', 0.03)
-sleep(0.4)
-username_prompt = typing_effect('\n            Enter your callsign below (between \x1b[93m2\033[0m and \x1b[93m15\033[0m characters):\n\n', 0.03) # Requests user to input 'callsign'
-username = input('                                 ')
+    sleep(0.6)
+    typing_effect('                         The enemy controls the Land.\n', 0.03)
+    typing_effect('                         The enemy controls the Skies.\n', 0.03)
+    typing_effect('            One Battleship stands between them controlling the Seas.\n', 0.03)
+    sleep(0.4)
+    username_prompt = typing_effect('\n            Enter your callsign below (between \x1b[93m2\033[0m and \x1b[93m15\033[0m characters):\n\n', 0.03) # Requests user to input 'callsign'
+    username = input('                                 ')
 validate_username_screen()                                                                                              # The steps to validate the username are called in this function
 mission_difficulty_screen()                                                                                                     # Displays the mission details to the user and prompts to accept  
 mission_accept_screen()
