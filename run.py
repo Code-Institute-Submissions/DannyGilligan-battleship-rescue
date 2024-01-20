0ed9617b9b2d5ff85cbb164de43e6dfe915733a5
DannyGilligan <gilligan.danny@gmail.com>
2024-01-20 17:01:06 +0000

    Add symbol legend to sitrep display

--- run.py
+++ run.py
@@ -457,10 +457,10 @@ def game_screen():
         print('--------------------- \x1b[96mBattleship Operations SITREP Display\033[0m ---------------------\n')
         #sleep(0.5)
         #clearscreen()
-        print(f'Torpedos remaining:       \x1b[96m{torpedo_count:02}\033[0m                       Missed shots:             \x1b[96m{(len(merchant_ship_locations)):02}\033[0m')   # Note for bug resolved, https://stackoverflow.com/questions/3505831/in-python-how-do-i-convert-a-single-digit-number-into-a-double-digits-string
+        print(f'Torpedos remaining:       \x1b[96m{torpedo_count:02}\033[0m                   (\x1b[90mX\x1b[0m) Missed shots:             \x1b[96m{(len(merchant_ship_locations)):02}\033[0m')   # Note for bug resolved, https://stackoverflow.com/questions/3505831/in-python-how-do-i-convert-a-single-digit-number-into-a-double-digits-string
         print(f'Hull armour remaining:    \x1b[96m{hull_armour_remaining:02}\033[0m                       Shot accuracy:            \x1b[96m{merchant_ships_destroyed:02}%\033[0m')
-        print(f'Enemy ships remaining:    \x1b[96m{(len(enemy_ship_locations)):02}\033[0m                       Enemy ships destroyed:    \x1b[96m{enemy_ships_destroyed:02}\033[0m')
-        print(f'Merchant ships remaining: \x1b[96m{(len(merchant_ship_locations)):02}\033[0m                       Merchant ships destroyed: \x1b[96m{merchant_ships_destroyed:02}\033[0m')
+        print(f'Enemy ships remaining:    \x1b[96m{(len(enemy_ship_locations)):02}\033[0m                   (\x1b[92mE\x1b[0m) Enemy ships destroyed:    \x1b[96m{enemy_ships_destroyed:02}\033[0m')
+        print(f'Merchant ships remaining: \x1b[96m{(len(merchant_ship_locations)):02}\033[0m                   (\x1b[93mM\x1b[0m) Merchant ships destroyed: \x1b[96m{merchant_ships_destroyed:02}\033[0m')
         print('\n')
 
 
