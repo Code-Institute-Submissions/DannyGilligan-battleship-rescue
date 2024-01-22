f5eb2769a198dab343556781a746f725a289429b
DannyGilligan <gilligan.danny@gmail.com>
2024-01-22 23:55:49 +0000

    Fix positioning of text in mission failure narrative

--- run.py
+++ run.py
@@ -850,7 +850,7 @@ def end_game_conditions():
     clearscreen()
     sleep(0.2)
     print('\033[?25l', end="")  # Code to hide cursor credited in README.md
-    typing_effect(f''' {username},
+    typing_effect(f'''    {username},
     We've received confirmation that the merchant ships have been destroyed.
     The cargo has been lost, there is nothing more we can do. Your orders are
     to stand down. All communication channels have been opened and made
@@ -863,9 +863,9 @@ def end_game_conditions():
     already reciprocated in kind. Our analysts predict that small pockets of
     humanity around the globe will survive the initial blasts and subsequent
     fallout in just enough numbers to prevent the extinction of our species.
-    \n May the survivors be granted wisdom beyond our own.\n\n''', 0.03)
+    \n    May the survivors be granted wisdom beyond our own.\n\n''', 0.03)
     sleep(2)
-    print(' God forive us.')
+    print('    God forive us.')
     print('\033[?25h', end="")  # Code to show cursor credited in README.md
 
 
