847d92bcf0a548d348ed11259eaf9f0a138a6fc2
DannyGilligan <gilligan.danny@gmail.com>
2024-01-22 23:45:28 +0000

    Test mission failure narrative on terminal screen

--- run.py
+++ run.py
@@ -850,22 +850,22 @@ def end_game_conditions():
     clearscreen()
     sleep(0.2)
     print('\033[?25l', end="")  # Code to hide cursor credited in README.md
-    typing_effect(f'''{username},
+    typing_effect(f''' {username},
     We've received confirmation that the merchant ships have been destroyed.
-    The cargo has been lost. There is nothing more we can do. Your orders are
-    to stand down. All communication channels have been opened and made avail\
-    able to the Meridian Queen. Have your crew contact their loved ones, or \
-    make peace in whichever way they choose.\n\nWe will lose this war. But so \
-    will the enemy.\n4 minutes ago the Oppenheimer Protocol was activated. \
-    Our last remaining ICBMs were equipped with the experimental 'Hades' \
-    warhead and are currently en route to targets of strategic value within \
-    enemy territory. The enemy has already reciprocated in kind. Our analysts \
-    predict that small pockets of humanity around the globe will survive the \
-    initial blast waves and subsequent fallout in just enough numbers to \
-    prevent the extinction of our species.\n\nMay the survivors \
-    be granted wisdom beyond our own.\n\n''', 0.03)
+    The cargo has been lost, there is nothing more we can do. Your orders are
+    to stand down. All communication channels have been opened and made
+    available to the Meridian Queen. Have your crew contact loved ones, or
+    make peace in whichever way they choose.\n
+    We have lost this war, but so has the enemy. 4 minutes ago the
+    Oppenheimer Protocol was activated. Our last remaining ICBMs were
+    equipped with the experimental 'Hades' warhead and are currently en route
+    to targets of strategic value within enemy territory. The enemy has
+    already reciprocated in kind. Our analysts predict that small pockets of
+    humanity around the globe will survive the initial blast waves and subsequent
+    fallout in just enough numbers to prevent the extinction of our species.
+    \n May the survivors be granted wisdom beyond our own.\n\n''', 0.03)
     sleep(2)
-    print('God forive us.')
+    print(' God forive us.')
     print('\033[?25h', end="")  # Code to show cursor credited in README.md
 
 
@@ -901,20 +901,21 @@ def main():
 #main()
 
 
-print('\033[?25l', end="") # Code to hide cursor credited in README.md
-typing_effect(f'''{username},
-We've received confirmation that the merchant ships have been destroyed.
-The cargo has been lost. There is nothing more we can do. Your orders are 
-to stand down. All communication channels have been opened and made available
-to the Meridian Queen. Have your crew contact their loved ones, or make peace 
-in whichever way they choose.\n\nWe will lose this war. But so will the enemy.\n
-4 minutes ago the Oppenheimer Protocol was activated. Our last remaining ICBMs 
-were equipped with the experimental 'Hades' warhead and are currently en route
-to targets of strategic value within enemy territory. The enemy has already 
-reciprocated in kind. Our analysts predict that small pockets of humanity around
-the globe will survive the initial blast waves and subsequent fallout in just 
-enough numbers to prevent the extinction of our species.\n\nMay the survivors \
-be granted wisdom beyond our own.\n\n''', 0.03)
+print('\033[?25l', end="")  # Code to hide cursor credited in README.md
+typing_effect(f''' {username},
+ We've received confirmation that the merchant ships have been destroyed.
+ The cargo has been lost, there is nothing more we can do. Your orders are
+ to stand down. All communication channels have been opened and made
+ available to the Meridian Queen. Have your crew contact loved ones, or
+ make peace in whichever way they choose.\n
+ We have lost this war, but so has the enemy. 4 minutes ago the
+ Oppenheimer Protocol was activated. Our last remaining ICBMs were
+ equipped with the experimental 'Hades' warhead and are currently en route
+ to targets of strategic value within enemy territory. The enemy has
+ already reciprocated in kind. Our analysts predict that small pockets of
+ humanity around the globe will survive the initial blast waves and subsequent
+ fallout in just enough numbers to prevent the extinction of our species.
+ \n May the survivors be granted wisdom beyond our own.\n\n''', 0.03)
 sleep(2)
-print('God forive us.')
-print('\033[?25h', end="") # Code to show cursor credited in README.md
+print(' God forive us.')
+print('\033[?25h', end="")  # Code to show cursor credited in README.md
